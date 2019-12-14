b55246f2bd78a11ac5f6eeeead35c31f488f0bd0
Emmabyrne36 <emmabyrne36@gmail.com>
2019-12-14 16:45:33 +0000

    Added login method

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -22,6 +22,8 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
             services.AddControllers ();
+            // Service created once per request in the scope
+            services.AddScoped<IAuthRepository, AuthRepository> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ README.md
@@ -0,0 +1,9 @@
+# .NET Core 3.1 API
+
+## API containing routes to register a user, log them in and check if a user exists in the SQLite DB.
+
+### Skills used: 
+- .NET Core 3.1 API
+- Repository Pattern
+- DTOs
+- Hashing algorithms to create passwords
